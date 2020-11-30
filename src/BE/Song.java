73b813d3cad086e5ab46ed06426e7da056c57b2b
DennisPC-bit <denn062g@easv365.dk>
2020-11-30 10:10:12 +0100

    Update Song.java

--- src/BE/Song.java
+++ src/BE/Song.java
@@ -1,4 +1,25 @@
 package BE;
 
 public class Song {
+    private String title;
+    private int year;
+
+    public Song(String title, int year){
+        this.title=title;
+        this.year=year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
