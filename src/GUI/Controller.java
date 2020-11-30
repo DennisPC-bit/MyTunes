4b8a763f22fbacfce9d15187bb66739d964a8494
DennisPC-bit <dnisp@live.dk>
2020-11-30 23:13:26 +0100

    made on actions

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,4 +1,68 @@
 package GUI;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+
 public class Controller {
+    public Slider volumeSlider;
+    public Label volumeSliverLabel;
+    public Button usePreviousButton;
+    public TextField searchField;
+    public TableView playlistTable;
+    public TableColumn playlistNameColumn;
+    public TableColumn playlistAmountOfSongsColumn;
+    public TableView songsOnPlaylistTable;
+    public TableColumn playlistSongsColumn;
+    public TableView songsTable;
+    public TableColumn songTableTitleColumn;
+    public TableColumn songTableArtistColumn;
+    public TableColumn songTableCategoryColumn;
+    public TableColumn songTableTimeColumn;
+    public Label currentSong;
+
+    public void addPlayListButton(ActionEvent actionEvent) {
+    }
+
+    public void editPlaylistButton(ActionEvent actionEvent) {
+    }
+
+    public void removeFromPlaylistButton(ActionEvent actionEvent) {
+    }
+
+    public void addToPlaylistButton(ActionEvent actionEvent) {
+    }
+
+    public void moveSongUpOnPlaylistButton(ActionEvent actionEvent) {
+    }
+
+    public void moveSongDownOnPlaylistButton(ActionEvent actionEvent) {
+    }
+
+    public void newSongButton(ActionEvent actionEvent) {
+    }
+
+    public void editSongButton(ActionEvent actionEvent) {
+    }
+
+    public void deleteSongButton(ActionEvent actionEvent) {
+    }
+
+    public void closeButton(ActionEvent actionEvent) {
+    }
+
+    public void search(KeyEvent keyEvent) {
+    }
+
+    public void clearSearchButton(ActionEvent actionEvent) {
+    }
+
+    public void playButton(ActionEvent actionEvent) {
+    }
+
+    public void nextButton(ActionEvent actionEvent) {
+    }
+
+    public void previousButton(ActionEvent actionEvent) {
+    }
 }
