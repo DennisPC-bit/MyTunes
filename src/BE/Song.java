5cb35661b82fdaf642af13b4801f2a3b94b2588d
DennisPC-bit <denn062g@easv365.dk>
2020-11-30 10:40:00 +0100

    Update Song.java

--- src/BE/Song.java
+++ src/BE/Song.java
@@ -1,25 +1,43 @@
 package BE;
 
-public class Song {
-    private String title;
-    private int year;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
-    public Song(String title, int year){
-        this.title=title;
-        this.year=year;
+public class Song {
+    private SimpleIntegerProperty id;
+    private SimpleIntegerProperty year;
+    private SimpleBooleanProperty visible;
+    private StringProperty title;
+
+    public Song(int id, String title, int year){
+        this.year=new SimpleIntegerProperty(id);
+        this.title = new SimpleStringProperty(title);
+        this.year = new SimpleIntegerProperty(year);
+        this.visible= new SimpleBooleanProperty(true);
     }
 
-    public int getYear() {
+    public SimpleIntegerProperty getYear() {
         return year;
     }
-    public String getTitle() {
+    public StringProperty getTitle() {
         return title;
+    }
+    public int getId() {
+        return id.get();
+    }
+    public boolean isVisible() {
+        return visible.get();
     }
 
     public void setTitle(String title) {
-        this.title = title;
+        this.title = new SimpleStringProperty(title);
     }
     public void setYear(int year) {
-        this.year = year;
+        this.year = new SimpleIntegerProperty(year);
+    }
+    public void setVisible(boolean visible) {
+        this.visible.set(visible);
     }
 }
